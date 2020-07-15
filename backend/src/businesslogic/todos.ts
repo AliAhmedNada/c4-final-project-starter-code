0f7277a895adf3a3f5141734c68836a37451a9e1
AliAhmedNada <aliahmedalihassan@gmail.com>
2020-07-15 23:11:32 +0200

    latest

--- backend/src/businesslogic/todos.ts
+++ backend/src/businesslogic/todos.ts
@@ -1,4 +1,3 @@
-
 import { TodoItem } from '../models/TodoItem'
 import { TodoAccess } from '../dataLayer/todoAccess'
 import { CreateTodoRequest } from '../requests/CreateTodoRequest'
@@ -9,71 +8,68 @@ import * as uuid from 'uuid';
 
 const todosAccess = new TodoAccess()
 
-export async function createTodo(
-  createTodoRequest: CreateTodoRequest,
-  jwtToken: string
-): Promise<TodoItem> {
+export async function createTodo(createTodoRequest: CreateTodoRequest,jwtToken: string): Promise<TodoItem> {
   const itemId = uuid.v4()
   const userId = parseUserId(jwtToken)
   return await todosAccess.createTodo({
-    todoId: itemId,
-    userId: userId,
-    name: createTodoRequest.name,
-    done: false,
-    dueDate: createTodoRequest.dueDate,
-    createdAt: new Date().toISOString()
+  todoId: itemId,
+  userId: userId,
+  name: createTodoRequest.name,
+  done: false,
+  dueDate: createTodoRequest.dueDate,
+  createdAt: new Date().toISOString()
   })
-}
+  }
 
-export async function getTodoItem(todoId: string, jwtToken: string): Promise<TodoItem> {
-  const userId = parseUserId(jwtToken)
-  return await todosAccess.getTodoItem(todoId, userId)
-}
-
-
-export async function getAllTodos(jwtToken: string): Promise<TodoItem[]> {
+  export async function getTodoItem(todoId: string, jwtToken: string): Promise<TodoItem> {
     const userId = parseUserId(jwtToken)
-    return todosAccess.getAllTodos(userId)
-}
+    return await todosAccess.getTodoItem(todoId, userId)
+    }
 
 
-export async function updateTodo(
-  todoId: string, 
-  updateTodoRequest: UpdateTodoRequest,
-  jwtToken: string
-): Promise<void> {
-  const userId = parseUserId(jwtToken)
-  console.log("Updating Item")
-  console.log(updateTodoRequest)
-  console.log(todoId)
-
-  const todoItem = await todosAccess.getTodoItem(todoId, userId)
-console.log('todoItem',todoItem);
-  await todosAccess.updateTodo(todoId,  todoItem.createdAt,{
-    name: updateTodoRequest.name,
-    done: updateTodoRequest.done,
-    dueDate: updateTodoRequest.dueDate,
-  })
-}
+    export async function getAllTodos(jwtToken: string): Promise<TodoItem[]> {
+      const userId = parseUserId(jwtToken)
+      return todosAccess.getAllTodos(userId)
+      }
 
 
-export async function deleteTodo(
-  itemId: string,
-  jwtToken: string
-): Promise<void> {
-  
-  const userId = parseUserId(jwtToken)
-  const todoItem = await todosAccess.getTodoItem(itemId, userId)
-  console.log('todoItem',JSON.stringify(todoItem))
-  await todosAccess.deleteTodo(todoItem.todoId, todoItem.createdAt)
-}
+      export async function updateTodo(
+      todoId: string,
+      updateTodoRequest: UpdateTodoRequest,
+      jwtToken: string
+      ): Promise<void> {
+        const userId = parseUserId(jwtToken)
+        console.log("Updating Item")
+        console.log(updateTodoRequest)
+        console.log(todoId)
+
+        const todoItem = await todosAccess.getTodoItem(todoId, userId)
+        console.log('todoItem',todoItem);
+        await todosAccess.updateTodo(todoId, todoItem.createdAt,{
+        name: updateTodoRequest.name,
+        done: updateTodoRequest.done,
+        dueDate: updateTodoRequest.dueDate,
+        })
+        }
 
 
-export async function generateUploadUrl(todoId: string, jwtToken: string): Promise<string> {
-  const userId = parseUserId(jwtToken)
-  console.log("Setting Item URL")
-  console.log(todoId)
-  console.log("userId:",userId)
+        export async function deleteTodo(
+        itemId: string,
+        jwtToken: string
+        ): Promise<void> {
 
-  return todosAccess.generateUploadUrl(todoId);
-}
+          const userId = parseUserId(jwtToken)
+          const todoItem = await todosAccess.getTodoItem(itemId, userId)
+          console.log('todoItem',JSON.stringify(todoItem))
+          await todosAccess.deleteTodo(todoItem.todoId, todoItem.createdAt)
+          }
+
+
+          export async function generateUploadUrl(todoId: string, jwtToken: string): Promise<string> {
+            const userId = parseUserId(jwtToken)
+            console.log("Setting Item URL")
+            console.log(todoId)
+            console.log("userId:",userId)
+
+            return todosAccess.generateUploadUrl(todoId);
+            }
--- backend/src/lambda/http/createTodo.ts
+++ backend/src/lambda/http/createTodo.ts
@@ -29,10 +29,7 @@ export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEven
       "Access-Control-Allow-Credentials": true
     },
     body: JSON.stringify({
-      item: {
-        todoId: newItem,
-        ...newTodo
-      }
+      item: newItem
     })
   }
 }
